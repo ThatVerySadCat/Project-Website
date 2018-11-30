fc119f404543c762ca16511c15101cb21d06be4d
Martijn van den Berk <thatverysadcat@gmail.com>
2018-11-30 14:11:54 +0100

    - Added the newly gotten bootstrap and its necesary dependancies (popper min and jquery slim min) to their own bundles.
    - Added the latest version of bootstrap.
    
    - Added the latest version of popper.
    
    - Added the Unity folder including all its necesary files.
    
    - Removed unnecesary html from the Index and _Layout files.

--- Project - Website/App_Start/BundleConfig.cs
+++ Project - Website/App_Start/BundleConfig.cs
@@ -22,6 +22,12 @@ namespace Project___Website
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/poppermin").Include(
+                "~/Scripts/umd/popper.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryslim").Include(
+                "~/Scripts/jquery-3.3.1.slim.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
