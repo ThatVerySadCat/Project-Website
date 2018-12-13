937ce1821e35f36b91157da8e3b68201e15236cd
Martijn van den Berk <thatverysadcat@gmail.com>
2018-12-13 10:29:08 +0100

    Added Logic Basis
    Added the projects necesary for the logic (Logic and Structs)
    
    Added the ScoreViewModel which has been filled with one variable, a list of scores.

---
+++ Project - Website/Models/ScoreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project___Website.Models
+{
+    public class ScoreViewModel
+    {
+        public List<Score> scores { get; set; }
+    }
+}
